fd3ed9598d5731ec96ec7954564dc7dd74a1c441
Oracle Public Cloud User <opc@instance-20260415-1657.sub10251124151.snvcn01.oraclevcn.com>
2026-06-02 14:49:31 +0000

    feat: staged changelogs for base_release

---
+++ dist/releases/next/changes/dev_base_release/myapp/ref_constraints/agreements_sponsor_fk.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1780411662067 stripComments:false  logicalFilePath:dev_base_release/myapp/ref_constraints/agreements_sponsor_fk.sql
+-- sqlcl_snapshot src/database/myapp/ref_constraints/agreements_sponsor_fk.sql:null:d6e6767aac61d141665511c3007a2182e7fd7000:create
+
+alter table sponsor_agreements
+    add constraint agreements_sponsor_fk
+        foreign key ( sponsor_id )
+            references sponsors ( sponsor_id )
+        enable;
+
---
+++ dist/releases/next/changes/dev_base_release/myapp/ref_constraints/agreements_team_fk.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1780411662094 stripComments:false  logicalFilePath:dev_base_release/myapp/ref_constraints/agreements_team_fk.sql
+-- sqlcl_snapshot src/database/myapp/ref_constraints/agreements_team_fk.sql:null:53cdfb5848d37237f5afa3d4d61da77c4b34a851:create
+
+alter table sponsor_agreements
+    add constraint agreements_team_fk
+        foreign key ( team_id )
+            references teams ( team_id )
+        enable;
+

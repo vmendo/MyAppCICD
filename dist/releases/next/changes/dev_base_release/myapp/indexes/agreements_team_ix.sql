0990108e045f2ddd89ccac3ef017e9d89e3a9c04
vmendo <20044697+vmendo@users.noreply.github.com>
2026-06-04 12:01:57 +0000

    feat: staged changelogs for base_release

---
+++ dist/releases/next/changes/dev_base_release/myapp/indexes/agreements_team_ix.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1780574505275 stripComments:false  logicalFilePath:dev_base_release/myapp/indexes/agreements_team_ix.sql
+-- sqlcl_snapshot src/database/myapp/indexes/agreements_team_ix.sql:null:0add3170606c5f4e29527f5c2e9affe48347209a:create
+
+create index agreements_team_ix on
+    sponsor_agreements (
+        team_id
+    );
+
---
+++ dist/releases/next/changes/dev_base_release/myapp/indexes/coaches_team_ix.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1780574505300 stripComments:false  logicalFilePath:dev_base_release/myapp/indexes/coaches_team_ix.sql
+-- sqlcl_snapshot src/database/myapp/indexes/coaches_team_ix.sql:null:acb66b14e63de36b98a05d8a7edcfa58b2390051:create
+
+create index coaches_team_ix on
+    coaches (
+        team_id
+    );
+

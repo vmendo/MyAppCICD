a2e5f83fac7c3d743af9ad92891878ef3ffa3d56
vmendo <20044697+vmendo@users.noreply.github.com>
2026-06-11 09:46:42 +0000

    feat: staged changelogs for base_release

---
+++ dist/releases/next/changes/dev_base_release/myapp/indexes/players_status_ix.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1781171199571 stripComments:false  logicalFilePath:dev_base_release/myapp/indexes/players_status_ix.sql
+-- sqlcl_snapshot src/database/myapp/indexes/players_status_ix.sql:null:bff8e3adc54d3498ad17e931752e203a2f95603d:create
+
+create index players_status_ix on
+    players (
+        roster_status
+    );
+
---
+++ dist/releases/next/changes/dev_base_release/myapp/views/vw_team_financial_summary.sql
@@ -0,0 +1,39 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1781171201186 stripComments:false  logicalFilePath:dev_base_release/myapp/views/vw_team_financial_summary.sql
+-- sqlcl_snapshot src/database/myapp/views/vw_team_financial_summary.sql:null:06678f51e3e28f15f13306e177bcd301d6f7d523:create
+
+create or replace force editionable view vw_team_financial_summary (
+    team_id,
+    financial_month,
+    revenue_amount,
+    expense_amount,
+    net_amount
+) as
+    select
+        team_id,
+        financial_month,
+        sum(revenue_amount)                       as revenue_amount,
+        sum(expense_amount)                       as expense_amount,
+        sum(revenue_amount) - sum(expense_amount) as net_amount
+    from
+        (
+            select
+                team_id,
+                trunc(revenue_date, 'MM') as financial_month,
+                amount                    as revenue_amount,
+                0                         as expense_amount
+            from
+                revenues
+            union all
+            select
+                team_id,
+                trunc(expense_date, 'MM') as financial_month,
+                0                         as revenue_amount,
+                amount                    as expense_amount
+            from
+                expenses
+        )
+    group by
+        team_id,
+        financial_month;
+

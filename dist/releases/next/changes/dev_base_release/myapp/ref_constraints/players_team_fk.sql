6daa99e296a38998a0f6f5835ba965e18ac4b1de
vmendo <20044697+vmendo@users.noreply.github.com>
2026-06-23 14:48:00 +0000

    feat: staged changelogs for base_release

---
+++ dist/releases/next/changes/dev_base_release/myapp/ref_constraints/players_team_fk.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1782225971784 stripComments:false  logicalFilePath:dev_base_release/myapp/ref_constraints/players_team_fk.sql
+-- sqlcl_snapshot src/database/myapp/ref_constraints/players_team_fk.sql:null:b0a25fd89617b81b8f42296861dcd22d77f8440f:create
+
+alter table players
+    add constraint players_team_fk
+        foreign key ( team_id )
+            references teams ( team_id )
+        enable;
+

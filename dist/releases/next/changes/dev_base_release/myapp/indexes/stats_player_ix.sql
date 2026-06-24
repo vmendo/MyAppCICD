33bff92caa05d8395311ba7ffe30390443e0c5d4
vmendo <20044697+vmendo@users.noreply.github.com>
2026-06-24 10:00:13 +0000

    feat: staged changelogs for base_release

---
+++ dist/releases/next/changes/dev_base_release/myapp/indexes/stats_player_ix.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1782295130408 stripComments:false  logicalFilePath:dev_base_release/myapp/indexes/stats_player_ix.sql
+-- sqlcl_snapshot src/database/myapp/indexes/stats_player_ix.sql:null:d2f3697ef56c9990c31f1d45d86e2344b2b3dd08:create
+
+create index stats_player_ix on
+    player_game_stats (
+        player_id
+    );
+

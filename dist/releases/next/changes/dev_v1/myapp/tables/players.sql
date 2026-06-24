b0ffeb98443a3acaac37929d108bd8ee9a69798a
vmendo <20044697+vmendo@users.noreply.github.com>
2026-06-24 10:11:03 +0000

    feat: staged changelogs for v1

---
+++ dist/releases/next/changes/dev_v1/myapp/tables/players.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+-- changeset MYAPP:1782295812542 stripComments:false  logicalFilePath:dev_v1/myapp/tables/players.sql
+-- sqlcl_snapshot src/database/myapp/tables/players.sql:1f995ddb0b9cec0f110c827eaec20d83626009f7:a406ca948d98df6dba4211cb3c58f7261d953177:alter
+
+alter table players add (
+    social_media_handle varchar2(80)
+)
+/
+
